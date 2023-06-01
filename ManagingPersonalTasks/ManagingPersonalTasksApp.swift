78b288c4dd1d62090ebe7c6b0bb3a6b436b3a6cc
mrzogy <magbolmrzogy@gmail.com>
2023-06-01 19:57:34 +0300

    Make branch

---
+++ ManagingPersonalTasks/ManagingPersonalTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManagingPersonalTasksApp.swift
+//  ManagingPersonalTasks
+//
+//  Created by 3bood on 12/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ManagingPersonalTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
